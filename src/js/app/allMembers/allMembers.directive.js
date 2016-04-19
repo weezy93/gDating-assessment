a404eed94459c17d2108abe8dda762d4fe35a21e
Louisa Reese <louisa.e.reese@gmail.com>
2016-04-19 15:34:25 -0600

    broke out allMembers to better configuration

--- src/js/app/allMembers/allMembers.directive.js
+++ src/js/app/allMembers/allMembers.directive.js
@@ -1,4 +1,31 @@
 (function () {
   angular.module('gChemistry')
-  .directive('allMembers')
+  .directive('allMembers', allMembers)
+  .controller('allMembersCtrl', allMembersCtrl);
+
+  allMembersCtrl.$inject = ['allMembersService'];
+
+
+  function allMembers() {
+    return {
+      templateUrl: 'js/app/allMembers/allMembers.template.html',
+      controller: allMembersCtrl,
+      controllerAs: 'vm'
+    };
+  };
+
+  function allMembersCtrl(allMembersService) {
+      var vm = this;
+      vm.member = {}
+
+      allMembersService.getAllMembers()
+      .then(function (data) {
+        vm.membersList = data;
+      });
+  };
+
 })();
+
+
+// register partial with controller and service
+// member dashboard
--- src/js/app/allMembers/allMembers.service.js
+++ src/js/app/allMembers/allMembers.service.js
@@ -1,11 +1,12 @@
 (function () {
+
   angular.module('gChemistry')
   .service('allMembersService', allMembersService);
 
   allMembersService.$inject = ['crudService'];
 
   function allMembersService(crudService) {
-    var service = {
+    return {
       getAllMembers: function () {
         return crudService.getAll('members')
         .then(function (members) {
@@ -14,14 +15,13 @@
         });
       },
       getOneMember: function (member) {
-        crudService.getOne('members', member)
+        return crudService.getOne('members', member)
         .then(function (member) {
           console.log(member);
           return member;
         });
       }
     };
-    return service;
   };
 
 })();
