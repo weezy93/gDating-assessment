824e346c719d6d5923ec277d5d85e9feee2b00c1
Louisa Reese <louisa.e.reese@gmail.com>
2016-04-21 19:52:58 -0600

    capitalize filter, formatted members page in grid

--- src/js/app/filters.js
+++ src/js/app/filters.js
@@ -13,9 +13,10 @@
         }
       }
     })
-    .filter('toUpperCase', function () {
-      return function (input) {
-       // make names to upper case
+    .filter('capitalize', function () {
+      return function (input, all) {
+        var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
+              return (!!input) ? input.replace(reg, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
       }
     });
 })();
